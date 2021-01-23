3b52ac420212bde399c65c59cfb5191f6c116bc9
bing <bing_yu2001@163.com>
2021-01-23 18:56:38 +0800

    操作基本页面布局

--- src/views/sys/SysAdmin.vue
+++ src/views/sys/SysAdmin.vue
@@ -1,18 +1,105 @@
 <template>
   <div>
-    操作员管理
+    <!-- 1、 -->
+    <div style="display: flex;justify-content: center;margin-top: 10px;">
+      <el-input placeholder="通过用户名搜索用户..." prefix-icon="el-icon-search"
+                style="width: 400px;margin-right: 10px;"></el-input>
+      <el-button type="primary" icon="el-icon-search">搜索</el-button>
+    </div>
+    <!-- 2、6、 -->
+    <div class="admin-container">
+      <el-card class="admin-card" v-for="(admin,index) in admins" :key="index">
+        <div slot="header" class="clearfix">
+          <span>{{ admin.name }}</span>
+          <el-button style="float: right; padding: 3px 0;color:red;" type="text" icon="el-icon-delete"></el-button>
+        </div>
+        <div>
+          <div class="img-container">
+            <img :src="admin.userFace" :alt="admin.name" :title="admin.name" class="userFace-img">
+          </div>
+        </div>
+        <div class="userinfo-container">
+          <div>用户名：{{ admin.name }}</div>
+          <div>手机号码：{{ admin.phone }}</div>
+          <div>电话号码：{{ admin.telephone }}</div>
+          <div>地址：{{ admin.address }}</div>
+          <div>用户状态：
+            <el-switch
+                v-model="admin.enabled"
+                active-color="#13ce66"
+                inactive-color="#ff4949"
+                active-text="启用"
+                inactive-text="禁用">
+            </el-switch>
+          </div>
+          <div>
+            用户角色：
+            <el-tag style="margin-right: 4px;" type="success" v-for="(role,index) in admin.roles" :key="index">
+              {{ role.nameZh }}
+            </el-tag>
+            <!-- 3个点按钮 ... -->
+            <el-button type="text" icon="el-icon-more"></el-button>
+          </div>
+          <div>备注：{{ admin.remark}}</div>
+        </div>
+      </el-card>
+    </div>
   </div>
 </template>
 <script>
 export default {
   name: "SysAdmin",
   data() {
-    return {}
+    return {
+      admins: [ // 3
+      ]
+    }
   },
-  methods: {}
+  mounted() {
+    this.initAdmins() // 5
+  },
+  methods: {
+    // 4、获取所有操作员
+    initAdmins() {
+      this.getRequest('/system/admin/').then(resp => {
+        if (resp) {
+          this.admins = resp
+        }
+      })
+    }
+  }
 }
 </script>
 
-<style scoped>
+<style>
+/* 7 */
+.admin-container {
+  margin-top: 10px;
+  display: flex;
+  justify-content: space-around; /* 在弹性盒对象的 <div> 元素中的各项周围留有空白：*/
+  flex-wrap: wrap; /* 自动换行 */
+}
 
+.admin-card {
+  width: 350px;
+  margin-bottom: 20px;
+}
+
+.userFace-img {
+  width: 72px;
+  height: 72px;
+  border-radius: 72px;
+}
+
+/* 头像居中 */
+.img-container {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+
+.userinfo-container {
+  font-size: 12px;
+  color: #3e9ef5;
+}
 </style>
